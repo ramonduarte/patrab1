094b786273a632bc9357e524b608531f8e810a7b
Ramon Melo <ramonduarte@poli.ufrj.br>
2019-06-16 22:56:40 -0300

    AJAX requests using jQuery.

--- mavenproject1/web/js/script.js
+++ mavenproject1/web/js/script.js
@@ -1,69 +1,67 @@
 // TODO: define urls 2019-06-13 19:14:32
-// urlmedidores
-
+// /mavenproject1/Ajax
+var $ = jQuery;
 
 $( document ).ready(function() {
-    $('#selectMedida').empty();
-    $.post({
-        url: "#urlmedidores",
-        data: { "medidores": "medidores" },
-        success: function(data){
-            var options = $.parseJSON(data);
-            $.each(options, function(i, d) {
-                $('#selectMedida').append('<option value="' + d.value + '">' + d.name + '</option>');
-            });
-        }
-    });
+    // $.post({
+    //     url: "/mavenproject1/Ajax",
+    //     data: { "medidores": "medidores" },
+    //     success: function(data){
+    //         $('#selectMedida').empty();
+    //         var options = $.parseJSON(data);
+    //         $.each(options, function(i, data) {
+    //             $('#selectMedida').append('<option value="' + data.value + '">' + data.name + '</option>');
+    //         });
+    //     }
+    // });
 
-    $("botaoLer").click(function (e) { 
+    $("#botaoLer").click(function (e) { 
         e.preventDefault();
         var medidor = $("#selectMedida").children("option:selected")[0].value;
         var periodo = $("#selectPeriodo").children("option:selected")[0].value;
-        var datafinal = $("#start").children("option:selected")[0].value;
-        var tabela = $("#selectTabela").children("option:selected")[0].value;
+        var datafinal = $("#start")[0].value;
+        // var tabela = $("#selectTabela")[0].value;
         $.post({
-            url: "#urlmedidores",
+            url: "/mavenproject1/Ajax",
             data: {
                 "medidor": medidor,
                 "periodo": periodo,
                 "datafinal": datafinal,
-                "tabela": tabela
+                // "tabela": tabela
             },
             success: function(data){
-                var options = $.parseJSON(data);
-                $.each(options, function(i, d) {
+                $('#tabelaMedidas tbody').remove();
+                $.each(data, function(i, data) {
                     // TODO: fix this, most likely 2019-06-13 21:52:49
-                    $('#tabelaMedidas').append("<tr><td>" + d + "</td></tr>");
+                    $('#tabelaMedidas').append("<tbody><tr><td>" + data["medidor"] + "</td><td>" +  + data["temperatura"] + "</td><td>" + data["umidade"] + "</td><td>" + data["datahora"] + "</td><td>" + data["serial"] + "</td></tr></tbody>");
                 });
             }
         });
     });
 
     // TODO: lista de sensores 2019-06-13 21:53:16
-    $('#selectMedida').empty();
-    $.post({
-        url: "#urlmedidores",
-        data: { "medidores": "medidores" },
-        success: function(data){
-            var options = $.parseJSON(data);
-            $.each(options, function(i, d) {
-                $('#selectMedida').append('<option value="' + d.value + '">' + d.name + '</option>');
-            });
-        }
-    });
+    // $.post({
+    //     url: "/mavenproject1/Ajax",
+    //     data: { "medidores": "medidores" },
+    //     success: function(data){
+    //         var options = $.parseJSON(data);
+    //         $.each(options, function(i, data) {
+    //             $('#selectMedida').append('<option value="' + data.value + '">' + data.name + '</option>');
+    //         });
+    //     }
+    // });
 
     // TODO: gráfico 2019-06-13 21:53:30
-    $('#selectMedida').empty();
-    $.post({
-        url: "#urlmedidores",
-        data: { "medidores": "medidores" },
-        success: function(data){
-            var options = $.parseJSON(data);
-            $.each(options, function(i, d) {
-                $('#selectMedida').append('<option value="' + d.value + '">' + d.name + '</option>');
-            });
-        }
-    });
+    // $.post({
+    //     url: "/mavenproject1/Ajax",
+    //     data: { "medidores": "medidores" },
+    //     success: function(data){
+    //         var options = $.parseJSON(data);
+    //         $.each(options, function(i, data) {
+    //             $('#selectMedida').append('<option value="' + data.value + '">' + data.name + '</option>');
+    //         });
+    //     }
+    // });
 
 
 
--- mavenproject1/web/pagina1.jsp
+++ mavenproject1/web/pagina1.jsp
@@ -13,6 +13,7 @@
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet" />
+        <script src="js/jquery-3.3.1.min.js"></script>
 
 
         <title>S.M.A.R.T. Home 2019® - Home Automation for the Nation</title>
@@ -45,7 +46,6 @@
                     <div class="form-group col">
                         <!-- <label for="selectMedida">Medida</label> -->
                         <select class="custom-select" id="selectMedida" name="medidor">
-                            <option selected value="">Medidor</option>
                                 <%  
                                     Class.forName("org.postgresql.Driver");
                                     Connection con = DriverManager.getConnection(
@@ -89,7 +89,8 @@
                         <span class="badge badge-primary" style="vertical-align: sub">Gráfico</span>
                     </div>
                     <div class="form-group col">
-                        <button id="botaoLer" type="submit" role="button" class="btn btn-primary">LER</button>
+                        <!-- <button id="botaoLer" type="submit" role="button" class="btn btn-primary">LER</button> -->
+                        <button id="botaoLer" type="button"role="button" class="btn btn-primary">LER</button>
                     </div>
                 </div>
             </form>
@@ -143,7 +144,6 @@
             
         </div>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script>
